7c4cda161edb1a3fc85bbe7dc1665b0b00a455ab
Parm345 <51205078+Parm345@users.noreply.github.com>
2020-07-13 15:40:44 -0400

    bunch of stuff

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,12 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
+    
     def __init__(self):
         self.chain = []
         self.current_transactions = []
@@ -65,3 +69,76 @@ class Blockchain(object):
         # We must make sure that the Dictioanry is Ordered, or we'll have incosisten hashes
         block_string = json.dumps(block, sort_keys=True).encode
         return hashlib.sha256(block_string).hexdigest()
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Alogrithm:
+         - Find a number p' such that has (pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+         :param last proof: <int>
+         :return: <int>
+         """
+
+        proof = 0
+
+        while self.valid_proof(last_proof, proof):
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does has(last_proof, proof) contain 4 leading zeroes
+        :param last_proof: <int> Current Proof
+        :return: <bool> True if correct, False if not
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('.', '')
+
+# Instantiate the Blochain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    # We run the proof of work alogrith to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof
+    # The sender is "0" to signify that his node mined a new coin
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # Chech that the required fields are in the POST'ed data
+    requried = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in requried):
+        return 'Missing values', 400
+
+    # Create a new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=500)
